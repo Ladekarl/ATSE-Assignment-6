84710be582c7f130657b5cebb8eaf21a17397155
Niels Ladekarl <nielsladekarl@gmail.com>
2018-03-13 10:10:09 +0100

    FotofApplication

---
+++ fotof/src/main/java/org/atse/project/fotof/FotofApplication.java
@@ -0,0 +1,9 @@
+package org.atse.project.fotof;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Fotof App")
+public class FotofApplication extends ServletProcessApplication {
+
+}
