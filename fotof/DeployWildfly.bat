0ebdcafdeae61eb21cbda96d95f642c7ce997af6
Niels Ladekarl <nielsladekarl@gmail.com>
2018-03-13 12:00:20 +0100

    Updated script to support wildfly

--- fotof/DeployWildfly.bat
+++ fotof/DeployWildfly.bat
@@ -1,2 +1,2 @@
 @echo off 
-xcopy /s /Y "target\fotof.war" "%JBOSS_HOME%\standalone\deployments"
+xcopy /s /Y "target\fotof.war" "%CAMUNDA_HOME%\server\wildfly-11.0.0.Final\standalone\deployments\"
--- fotof/src/main/java/org/atse/project/fotof/FotofApplication.java
+++
@@ -1,9 +0,0 @@
-package org.atse.project.fotof;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Fotof App")
-public class FotofApplication extends ServletProcessApplication {
-
-}
